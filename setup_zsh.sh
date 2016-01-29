313bf20900dfac9e2b0629d5669bb3729716a1c0
Paul MOTTE <pmotte@e1r1p3.42.fr>
2016-01-29 13:29:55 +0100

    adding zshrc and meslo font

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 zsh --version # Check installation success by displaying version
-
 chsh -s $(which zsh) # Make zsh the default shell
+
+cp zshrc $HOME/.zshrc
+
+
+echo "Install powerfont meslo and change the font for your profile in iTerm"
+
+open "Meslo LG M Regular for Powerline.otf" 
